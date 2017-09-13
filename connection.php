6a9e775880733714e0d6a63ddb0759e8cdb55311
remilefebvre <lefebvre.reminicolas@gmail.com>
2017-09-13 15:21:31 +0200

    first commit & add template

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+try
+{
+  // On se connecte à MySQL
+  $bdd = new PDO('mysql:host=localhost;dbname=openclassroom;charset=utf8', 'root', 'gesundheititit',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e)
+{
+  // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+?>
