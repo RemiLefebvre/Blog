ba004d45475bc64a50927a4cde9ebefc8bc7139e
remilefebvre <lefebvre.reminicolas@gmail.com>
2017-09-14 12:14:20 +0200

    add com system

--- index.php
+++ index.php
@@ -27,37 +27,47 @@
     </head>
 
     <body>
-      <div class="card container">
-        <form class="d-flex" action="post.php" method="post">
-          <div class="">
-            <input type="text" name="titre" placeholder="Titre">
-            <textarea name="contenu" rows="8" cols="80"></textarea>
-          </div>
-          <input type="submit" value="Poster">
-        </form>
+      <div class="container">
+
+        <h3>Ajouter post </h3>
+        <div class="card">
+          <form class="d-flex" action="post.php" method="post">
+            <div class="">
+              <input type="text" name="titre" placeholder="Titre">
+              <textarea name="contenu" rows="8" cols="80"></textarea>
+            </div>
+            <input type="submit" value="Poster">
+          </form>
+        </div>
       </div>
 
-        <h1>SUPER BLOG 2000</h1>
-        <?php
+        <div class="container">
 
-        $req=$reponse = $bdd->query('SELECT * FROM billets') or die(print_r($bdd->errorInfo()));
-
-        while ($donnees = $reponse->fetch()) {
-          ?>
-          <div class="card container">
-            <div class="d-flex">
-              <h3><?php echo $donnees['titre'] ?></h3>
-              <h5 class="ml-auto "><?php echo $donnees['date_post'] ?></h5>
-            </div>
-            <?php echo $donnees['contenu'] ?>
-          </div>
+          <h1 class="mt-5">SUPER BLOG 2000</h1>
           <?php
-        }
 
-        $reponse->closeCursor(); // Termine le traitement de la requête
+          $reponse = $bdd->query('SELECT * FROM billets ORDER BY ID DESC') or die(print_r($bdd->errorInfo()));
 
-        ?>
-      </main>
+          while ($donnees = $reponse->fetch()) {
+            ?>
+            <div class="card mt-5">
+              <div class="d-flex">
+                <h3><?php echo $donnees['titre'] ?></h3>
+                <h5 class="ml-auto "><?php echo $donnees['date_post'] ?></h5>
+              </div>
+              <?php echo $donnees['contenu'] ?>
+              <?php echo '<br>' ?>
+              <?php
+                echo '<a href="comment.php"> ' . $donnees['nb_com'] . ' commentaires </a>';
+                ?>
+            </div>
+            <?php
+          }
+
+          $reponse->closeCursor(); // Termine le traitement de la requête
+
+          ?>
+        </div>
 
       </script>
       <script src="js/jquery.min.js"></script>
